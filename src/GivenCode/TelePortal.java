5530882b973c60b9116ebd59110dc604557edfc9
Abra-web <65129853+Abra-web@users.noreply.github.com>
2022-02-02 12:55:45 +0100

    I added the given code they gave (just for checking / didn't make it work though (should it?) / there are 2 different main files given at zip file (added Explorer one). Created src,test package

---
+++ src/GivenCode/TelePortal.java
@@ -0,0 +1,30 @@
+package GivenCode;
+
+public class TelePortal extends Area{
+    protected int yTarget;
+    protected int xTarget;
+    protected double outOrientation;
+
+    public TelePortal(int x1, int y1, int x2, int y2, int targetX, int targetY){
+        super(x1,y1,x2,y2);
+        yTarget=targetY;
+        xTarget=targetX;
+        outOrientation = 0.0;
+    }
+
+    public TelePortal(int x1, int y1, int x2, int y2, int targetX, int targetY, double orient){
+        super(x1,y1,x2,y2);
+        yTarget=targetY;
+        xTarget=targetX;
+        outOrientation = orient;
+    }
+
+    public int[] getNewLocation(){
+        int[] target = new int[] {yTarget,xTarget};
+        return target;
+    }
+
+    public double getNewOrientation(){
+        return outOrientation;
+    }
+}
