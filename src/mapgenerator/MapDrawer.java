72eacb7aa5ef238392c064584ad0d30037e9585a
DIno <01dino@live.nl>
2022-02-09 14:51:36 +0100

    Changes to wall creation

--- src/mapgenerator/MapDrawer.java
+++ src/mapgenerator/MapDrawer.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 public class MapDrawer extends JFrame implements MouseListener {
@@ -15,9 +16,9 @@ public class MapDrawer extends JFrame implements MouseListener {
     private static final double VERTICAL_BORDER = 100;
     private static final double frameWidth = 600;
     private static final double frameHeight = 400;
-    private final ArrayList<Line2D> walls;
+    private final ArrayList<Rectangle2D> walls;
     private final ArrayList<Line2D> borders;
-    public MapDrawer(ArrayList<Line2D> walls, ArrayList<Line2D> borders){
+    public MapDrawer(ArrayList<Rectangle2D> walls, ArrayList<Line2D> borders){
         setLayout(new BorderLayout());
         JPanel panel = new JPanel();
         add(panel, BorderLayout.CENTER);
@@ -33,12 +34,12 @@ public class MapDrawer extends JFrame implements MouseListener {
 
     }
 
-    private static void scaleSize(ArrayList<Line2D> walls, double mapWidth, double mapHeight) {
+    private static void scaleSize(ArrayList<Rectangle2D> walls, double mapWidth, double mapHeight) {
         double widthMultiplier = frameWidth/mapWidth;
         System.out.println(widthMultiplier);
         double heightMultiplier = frameHeight/mapHeight;
-        for (Line2D line : walls) {
-            line.setLine((line.getX1() * widthMultiplier) +  (HORIZONTAL_BORDER/2), (line.getY1() * heightMultiplier) + (VERTICAL_BORDER/2), (line.getX2() * widthMultiplier) +  (HORIZONTAL_BORDER/2), (line.getY2() * heightMultiplier) + (VERTICAL_BORDER/2));
+        for (Rectangle2D line : walls) {
+            //line.setLine((line.getX1() * widthMultiplier) +  (HORIZONTAL_BORDER/2), (line.getY1() * heightMultiplier) + (VERTICAL_BORDER/2), (line.getX2() * widthMultiplier) +  (HORIZONTAL_BORDER/2), (line.getY2() * heightMultiplier) + (VERTICAL_BORDER/2));
 
         }
     }
@@ -54,14 +55,14 @@ public class MapDrawer extends JFrame implements MouseListener {
         }
 
         g.setColor(Color.RED);
-        for (Line2D lin : walls) {
-            g2.draw(lin);
+        for (Rectangle2D rect : walls) {
+            g2.draw(rect);
         }
 
     }
 
     public static void main(String[] args){
-        ArrayList<Line2D> list = SimpleMapReader.readInCoordinatesOfWall();
+        ArrayList<Rectangle2D> list = SimpleMapReader.readInCoordinatesOfWall();
         double mapWidth = SimpleMapReader.readInConstant("width");
         double mapHeight = SimpleMapReader.readInConstant("height");
 
@@ -118,7 +119,11 @@ public class MapDrawer extends JFrame implements MouseListener {
         wall = new StringBuilder();
         tempLine[2] = e.getX();
         tempLine[3] = e.getY();
-        walls.add(new Line2D.Double(tempLine[0],tempLine[1],tempLine[2],tempLine[3]));
+        double xRect = Math.min(tempLine[0], tempLine[2]);
+        double wRect = Math.max(tempLine[0], tempLine[2]) - xRect;
+        double yRect = Math.min(tempLine[1], tempLine[3]);
+        double hRect = Math.max(tempLine[1], tempLine[3]) - yRect;
+        walls.add(new Rectangle2D.Double(xRect, yRect, wRect, hRect));
         repaint();
 
     }
