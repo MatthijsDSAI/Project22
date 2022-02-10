e325bb6a8722d512a7a8aef11a8726efd4a2fbc1
Bzz77 <72268548+Bzz77@users.noreply.github.com>
2022-02-10 15:07:47 +0100

    Update GUI

--- src/GUI/MapGui.java
+++ src/GUI/MapGui.java
@@ -1,8 +1,8 @@
 package GUI;
 
-import controller.Area;
-import controller.Scenario;
-import controller.TelePortal;
+import GivenCode.Area;
+import GivenCode.Scenario;
+import GivenCode.TelePortal;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -11,9 +11,11 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class MapGui extends Application {
 
-    private Scenario scenario;
+    private Scenario scenario = new Scenario("testmap.txt");
     private int mapHeight;
     private int mapWidth;
     private double scaling;
@@ -31,7 +33,7 @@ public class MapGui extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Pane root = createPane();
-        Scene scene = new Scene(root, 600, 600);
+        Scene scene = new Scene(root, 1200, 700);
         stage.setTitle("MAP");
         stage.setScene(scene);
         stage.show();
@@ -44,45 +46,57 @@ public class MapGui extends Application {
         Rectangle r1 = null;
         for(Area wall : scenario.getWalls()){
             r1 = wall.createRec();
-            r1.setFill(Color.WHEAT);
+            r1.setFill(Color.BLACK);
             p.getChildren().add(r1);
         }
 
         Rectangle r2 = null;
         for(TelePortal t : scenario.getTeleportals()){
             r2 = t.createRec();
-            r2.setFill(Color.NAVY);
+            r2.setFill(Color.YELLOW);
             p.getChildren().add(r2);
         }
 
         Rectangle r3 = null;
         for(Area s : scenario.getShaded()){
             r3 = s.createRec();
-            r3.setFill(Color.GREEN);
+            r3.setFill(Color.GREY);
             p.getChildren().add(r3);
         }
 
         //Setting spawn point and target
         Rectangle spawnAreaGuards = scenario.getSpawnAreaGuards().createRec();
+        spawnAreaGuards.setFill(Color.BLUE);
         p.getChildren().add(spawnAreaGuards);
 
-        Rectangle spawnAreaIntruders = scenario.getSpawnAreaIntruders().createRec();
-        p.getChildren().add(spawnAreaIntruders);
+        //The spawn area of intruders have the same location as the guards for testmap
+//        Rectangle spawnAreaIntruders = scenario.getSpawnAreaIntruders().createRec(scaling);
+//        spawnAreaIntruders.setFill(Color.BROWN);
+//        p.getChildren().add(spawnAreaIntruders);
 
         Rectangle targetArea = scenario.getTargetArea().createRec();
+        targetArea.setFill(Color.RED);
         p.getChildren().add(targetArea);
 
         //Setting Guards
         double[][] spawnGuards = scenario.spawnGuards();
         for(int i=0; i< scenario.getNumGuards(); i++){
-            Circle c = new Circle((int) spawnGuards[i][0], (int)spawnGuards[i][1], (int) spawnGuards[i][2]);
+            Circle c = new Circle();
+            c.setCenterX(spawnGuards[i][0]);
+            c.setCenterY(spawnGuards[i][1]);
+            c.setRadius(5);
+            c.setFill(Color.BLACK);
             p.getChildren().add(c);
         }
 
         //Setting intruders
         double[][] spawnIntruders = scenario.spawnIntruders();
         for(int i=0; i< scenario.getNumIntruders(); i++){
-            Circle c = new Circle((int) spawnIntruders[i][0], (int)spawnIntruders[i][1], (int) spawnIntruders[i][2]);
+            Circle c = new Circle();
+            c.setCenterX(spawnIntruders[i][0]);
+            c.setCenterY(spawnIntruders[i][1]);
+            c.setRadius(5);
+            c.setFill(Color.GREEN);
             p.getChildren().add(c);
         }
 
