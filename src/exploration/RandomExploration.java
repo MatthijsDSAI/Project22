0d1eb5d04b82c020521810a0b974e06acb6411c7
RW8440 <r.withaar@student.maastrichtuniversity.nl>
2022-03-03 16:03:32 +0100

    Random exploration class added

--- src/exploration/RandomExploration.java
+++ src/exploration/RandomExploration.java
@@ -5,21 +5,14 @@ import static java.lang.Math.*;
 public class RandomExploration {
 
     public static int randomMove() {
-        int randomMove = (int) (Math.random()*360);
+        int randomMove = (int) (Math.random() * 360);
         if (randomMove < 90) {
             return 0;
-        }   else if (randomMove < 180) {
+        } else if (randomMove < 180) {
             return 90;
-        }   else if (randomMove < 270) {
+        } else if (randomMove < 270) {
             return 180;
-        }   else return 270;
-
-    }
-
-
-    public static void main(String[] args){
-        int randomMove = randomMove();
-        System.out.println(randomMove);
+        } else return 270;
     }
 
 }
