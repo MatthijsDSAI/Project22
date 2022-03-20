a4ed8d496178705d74089321b4f935ef165efe78
Alexandra Panghe <72268989+adap7@users.noreply.github.com>
2022-03-20 15:36:54 +0100

    Add files via upload

---
+++ src/exploration/Exploration.java
@@ -0,0 +1,15 @@
+package exploration;
+
+import agents.Agent;
+import controller.Map.Map;
+import controller.Map.tiles.Tile;
+import utils.DirectionEnum;
+
+public abstract class Exploration {
+
+    public Exploration() {}
+
+    public abstract DirectionEnum makeMove();
+    public abstract String getExplorationName();
+
+}
