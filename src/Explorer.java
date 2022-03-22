a521aee42835b1bc330f32cad292d4c4c6c460a8
Dino <dpasic8@gmail.com>
2022-03-22 23:06:02 +0100

    gradle

--- src/Explorer.java
+++ src/Explorer.java
@@ -2,12 +2,9 @@ import controller.GameRunner;
 import controller.Scenario;
 
 public class Explorer {
-    protected String mapDoc;
-    protected Scenario scenario;
-
 
     public static void main(String[] args){
-        String mapD="testmap.txt";
+        String mapD="testmap2.txt";
         Scenario scenario = new Scenario(mapD);
         GameRunner gr = new GameRunner(scenario);
     }
