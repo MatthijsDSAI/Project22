ea4567c6008f5260625e8f470a7f379abca54775
Dino <dpasic8@gmail.com>
2022-06-05 15:40:03 +0200

    .

--- src/Explorer.java
+++ src/Explorer.java
@@ -4,7 +4,7 @@ import controller.Scenario;
 public class Explorer {
 
     public static void main(String[] args){
-        String mapD="testAngle.txt";
+        String mapD="testmap1.txt";
         Scenario scenario = new Scenario(mapD);
         GameRunner gr = new GameRunner(scenario);
     }
